c7fb59c1afd1533c7b7045d2bbf0781a46d9530e
yiyzhang <jameszhang_2005@yahoo.com>
2016-11-17 22:47:59 -0600

    first commit

---
+++ CreateDeviceIdentity/CreateDeviceIdentity/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=JZ-HUB1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=c5z3GWBRpQekSqu6s1rQMX46QtoTNJzneJZKzmwYpmc=";
+        //Generated device key: ULh9O5Ubebc4KQPHH1M3QJNx9mqXoWtVjTREe/FxF6g=
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myDevice1";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
