a31fce153c7f3ede62e09a347d248ae7abcb9b29
yiyzhang <jameszhang_2005@yahoo.com>
2016-11-19 09:30:22 -0600

    added 2 projects

---
+++ CreateDeviceIdentity/SimulatedDevice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "JZ-HUB1.azure-devices.net";
+        static string deviceKey = "ULh9O5Ubebc4KQPHH1M3QJNx9mqXoWtVjTREe/FxF6g="; //provisioned from the first project
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myDevice1", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myDevice1",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(5000).Wait();
+            }
+        }
+    }
+}
